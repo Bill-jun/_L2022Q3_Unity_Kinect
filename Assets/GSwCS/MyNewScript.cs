a1d2e9732ab99971c11d3a60ef447c745e4eede8
lectures202x <lectures.202x@gmail.com>
2022-09-20 11:20:19 +0900

    20220920

---
+++ Assets/GSwCS/MyNewScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyNewScript : MonoBehaviour
+{
+    int i = 10;
+    public GameObject MyCube;
+
+    void Start()
+    {
+        string name = MyCube.name;
+        print(name);
+    }
+}
